2e7111d6e5f69d58aea67fb3b921fb174aa8ad04
Erich Snow <ewsnow@usfca.edu>
2015-10-06 21:28:54 -0700

    Removed a print line - clean

--- tea_temp.py
+++ tea_temp.py
@@ -3,5 +3,4 @@ temperature = input('Enter a value between 206 and 212: ')
 while temperature > 205:
     print (temperature)
     temperature = temperature - 1
-    print ('Let\'s add an ice cube.')
 print ('The tea is cool enough to drink now.')
