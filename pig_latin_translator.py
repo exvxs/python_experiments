131eba203cb45366f6fe617f791e7cf816d12141
Erich Snow <erichsnow@erichs-air.attlocal.net>
2015-10-06 21:05:56 -0700

    Made the code repeatable, added a 'Quit' option

--- pig_latin_translator.py
+++ pig_latin_translator.py
@@ -1,11 +1,14 @@
 print 'Welcome to the Pig Latin Translator!'
 original = raw_input('Enter a word: ')
-pyg = 'ay'
 word = original.lower()
-first = word[0]
-new_word = word + first + pyg
-new_word = new_word[1:len(new_word)]
-if len(new_word) > 0 and new_word.isalpha():
-    print new_word
-else:
-    print 'I\'m sorry, please try again.'
+while word != 'quit':
+    first = word[0]
+    pyg = 'ay'
+    new_word = word + first + pyg
+    new_word = new_word[1:len(new_word)] # can write also [1:]
+    if len(new_word) > 0 and new_word.isalpha():
+        print new_word
+    else:
+        print 'I\'m sorry, please try again.' # prepare variable or variables for the next time through the loop
+    word = raw_input('Enter a word: ')
+    word = word.lower()
